ccf89ecb0fd55c40239c07511b7b3c826e4ea3fe
beeadventure <fuzeaisol@gmail.com>
2025-11-19 22:36:07 +0700

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,5 @@
-import Header from "@/components/header"
-import FloatingButtons from "@/components/floating-buttons"
+import { Header } from "@/components/header"
+import { FloatingButtons } from "@/components/floating-buttons"
 
 export default function AboutPage() {
   return (
