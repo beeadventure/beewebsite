33451682d91b9d956b9e5e7a3d3199e3c97af197
beeadventure <fuzeaisol@gmail.com>
2025-11-19 22:05:48 +0700

    Update page.tsx

--- app/fr/page.tsx
+++ app/fr/page.tsx
@@ -1,9 +1,9 @@
-import { Header } from "@/components/header"
-import { HeroFr } from "@/components/fr/hero-fr"
-import { ToursFr } from "@/components/fr/tours-fr"
-import { AboutFr } from "@/components/fr/about-fr"
-import { ContactFr } from "@/components/fr/contact-fr"
-import { FloatingButtons } from "@/components/floating-buttons"
+import Header from "@/components/header"
+import HeroFr from "@/components/fr/hero"
+import ToursFr from "@/components/fr/tours"
+import AboutFr from "@/components/fr/about"
+import ContactFr from "@/components/fr/contact"
+import FloatingButtons from "@/components/floating-buttons"
 
 export default function HomeFr() {
   return (
